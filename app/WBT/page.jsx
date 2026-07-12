8656508d4863a5169a2546612210036d3b7bc826
glynne <glynne@MacBook-Pro-de-glynne.local>
2026-07-12 13:37:52 -0500

    ‘saPS’

--- app/WBT/page.jsx
+++ app/WBT/page.jsx
@@ -6,8 +6,8 @@ import Main1 from './components/main';
 export default function Home() {
   return (
     <main className="min-h-[90vh] bg-[#FFF] text-[#242424] p-8 md:p-16">
-      
-      {/* Contenedor Sección Hero / Principal */}
+
+      {/*  */}
       <section className="w-full ">
         <Main1 />
       </section>
